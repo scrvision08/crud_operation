8a047b9b744e05ff42d68a0f64f8bb450ee4e872
scrvision08 <sauravchaudhari714@gmail.com>
2026-07-14 08:42:34 +0530

    forth commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     firstname: "",
     lastName: "",
     mobile: "",
-    course: "Web Developer",
+    course: "",
     gender: "",
   });
   const [studentData, setStudentData] = useState(()=>{
